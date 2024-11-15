ddd3afed35cc60714343db73fc9d6ba14f4ff108
Dan Sadaka <dan@web-site1.com>
2024-11-15 15:12:50 -0500

    Now listing prouducts in react frontend

--- app/frontend/App.tsx
+++ app/frontend/App.tsx
@@ -1,10 +1,41 @@
 import { FC } from 'react';
+import gql from 'graphql-tag';
+import { useGQLQuery } from "./useGQLQuery";
+
+const GET_PRODUCTS = gql`
+    query ListProducts {
+      products {
+        id
+        name
+        type
+        length
+        width
+        height
+        weight
+      }
+    }
+`
+// const App: FC = () => {
+//   return (
+//     <div>
+//       <h1>Hello, World!</h1>
+//     </div>
+// )};
 
 const App: FC = () => {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-)};
+    const { data, isLoading, error } = useGQLQuery(['products'], GET_PRODUCTS)
+    console.log(data)
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Something went wrong ...</div>;
+
+    return (
+        <div>
+          <p>Product Listing</p>
+            {data.products.map(product => (
+                <div key={product.name}>{product.name}</div>
+            ))}
+        </div>
+    );
+};
 
 export default App;
--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactTable } from "@tanstack/react-table";
 import App from 'App';
 import 'virtual:windi.css';
 
 const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <QueryClientProvider client={ queryClient }>
+          <App />
+      </QueryClientProvider>
+  </React.StrictMode>,
 );
